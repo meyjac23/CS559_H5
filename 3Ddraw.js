422f5a4633fc75e15d63a3f08e4b39f99c290a1f
meyjac16 <jmeyer32@wisc.edu>
2020-11-11 18:26:55 -0600

    Final "3-4" submission

--- 3Ddraw.js
+++ 3Ddraw.js
@@ -6,9 +6,15 @@ function setup() {
     var vec3 = glMatrix.vec3;
     var context = canvas.getContext('2d');
     var tparam = 0;
+    var ang_slider = document.getElementById("cam_slider");
+    ang_slider.value = 0;
+    var dist_slider = document.getElementById("dist_slider");
+    dist_slider.value = 0;
 
     function draw() {
         canvas.width = canvas.width; // Reset the screen
+        var viewAngle = ang_slider.value*0.02*Math.PI;
+        var dis = dist_slider.value*0.5;
 
         function moveToTx(loc, Tx) {
             var res=vec3.create(); vec3.transformMat4(res,loc,Tx); context.moveTo(res[0],res[1]);
@@ -19,6 +25,7 @@ function setup() {
         }
 
         function drawRocket(color, TxU) {
+            var Tx = mat4.clone(TxU);
             context.beginPath();
             context.fillStyle = "#a3a3a3";
             context.ellipse(0, 0, 5, 15, Math.PI, 0, 2*Math.PI);
@@ -32,22 +39,36 @@ function setup() {
             context.fill();
             context.stroke();
             context.beginPath();
-            moveToTx([-3, -12], TxU);
-            lineToTx([3, -12], TxU);
-            lineToTx([0, -18], TxU);
+            moveToTx([-3, -12], Tx);
+            lineToTx([3, -12], Tx);
+            lineToTx([0, -18], Tx);
             context.closePath();
             context.fillStyle = color;
             context.fill();
             context.beginPath();
-            moveToTx([-3, 12], TxU);
-            lineToTx([-5, 17], TxU);
-            lineToTx([-1, 14], TxU);
-            lineToTx([1, 14], TxU);
-            lineToTx([5, 17], TxU);
-            lineToTx([3, 12], TxU);
+            moveToTx([-3, 12], Tx);
+            lineToTx([-5, 17], Tx);
+            lineToTx([-1, 14], Tx);
+            lineToTx([1, 14], Tx);
+            lineToTx([5, 17], Tx);
+            lineToTx([3, 12], Tx);
             context.closePath();
             context.fill();
         }
+
+        function drawObject(color,TxU,scale) {
+            var Tx = mat4.clone(TxU);
+            mat4.scale(Tx,Tx,[scale,scale,scale]);
+            context.beginPath();
+	        context.fillStyle = color;
+	        moveToTx([-0.05, -0.1, 0], Tx);
+	        lineToTx([-0.05, 0.2, 0], Tx);
+	        lineToTx([0, 0.25, 0], Tx);
+	        lineToTx([0.05, 0.2, 0], Tx);
+	        lineToTx([0.05, -0.1, 0], Tx);
+	        context.closePath();
+	        context.fill();
+	    }
 
         var Hermite = function(t) {
             return [
@@ -75,8 +96,8 @@ function setup() {
             return result;
         }
 
-        var p_ = [[100, 80, 60], [-50, 40, 80], [-60, 50, -60], [20, 70, -50], [40, 50, 20]];
-        var d_ = [[-10, -10, 50], [-20, 0, -40], [0, 25, -50], [50, 10, 0], [20, 0, 30]];
+        var p_ = [[40, 80, 100], [-80, 40, 80], [-60, 150, -60], [20, 70, -50], [40, 50, 20]];
+        var d_ = [[0, -80, 120], [-20, 0, 140], [110, 125, -150], [150, 110, 0], [120, 0, 130]];
         var P_ = [[p_[0], d_[0], p_[1], d_[1]], [p_[1], d_[1], p_[2], d_[2]], [p_[2], d_[2], p_[3], d_[3]], [p_[3], d_[3], p_[4], d_[4]], [p_[4], d_[4], p_[0], d_[0]]];
 
         var C0 = function (t) {return Cubic(Hermite, P_[0], t);};
@@ -131,6 +152,15 @@ function setup() {
             }
         };
 
+        var CameraCurve = function(angle) {
+            var distance = 120.0;
+            var eye = vec3.create();
+            eye[0] = distance*Math.sin(viewAngle);
+            eye[1] = dis;
+            eye[2] = distance*Math.cos(viewAngle);
+            return [eye[0],eye[1],eye[2]];
+        };
+
         function drawTrajectory(t_begin,t_end,intervals,C,Tx,color) {
 	        context.strokeStyle=color;
 	        context.beginPath();
@@ -143,8 +173,11 @@ function setup() {
 	    }
 
 	    // Create Camera
-        var eye_cam = vec3.fromValues(0, 0, 0);
-        var target_cam = Ccomp(tparam);
+        //var eye_cam = Ccomp(tparam);
+        //var eye_cam = vec3.fromValues(-150, 50, -150);
+        var eye_cam = CameraCurve(viewAngle);
+        //var target_cam = Ccomp(tparam);
+        var target_cam = vec3.fromValues(0, 0, 0);
         var up_cam = vec3.fromValues(0, 1, 0);
         var Tlookat = mat4.create();
         mat4.lookAt(Tlookat, eye_cam, target_cam, up_cam);
@@ -167,24 +200,27 @@ function setup() {
         var Trocket = mat4.create();
         mat4.fromTranslation(Trocket, Ccomp(tparam));
         var tangent = Ccomp_tan(tparam);
-        var angle = Math.atan2(tangent[1], tangent[0]);
-        //mat4.rotateZ(Trocket, Trocket, angle);
+        var x_y_angle = Math.atan2(tangent[0], tangent[1]);
+        mat4.rotateX(Trocket, Trocket, x_y_angle);
+
 
         // Combine everything
         var TView_Proj_Cam_Model = mat4.create();
         mat4.multiply(TView_Proj_Cam_Model, TView_Proj_Cam, Trocket);
 
-        drawTrajectory(0.0, 1.0, 100, C0, TView_Proj_Cam_Model, "green");
-        drawTrajectory(0.0, 1.0, 100, C1, TView_Proj_Cam_Model, "green");
-        drawTrajectory(0.0, 1.0, 100, C2, TView_Proj_Cam_Model, "green");
-        drawTrajectory(0.0, 1.0, 100, C3, TView_Proj_Cam_Model, "green");
-        drawTrajectory(0.0, 1.0, 100, C4, TView_Proj_Cam_Model, "green");
-        drawRocket("red", TView_Proj_Cam_Model);
+        drawTrajectory(0.0, 1.0, 100, C0, TView_Proj_Cam, "green");
+        drawTrajectory(0.0, 1.0, 100, C1, TView_Proj_Cam, "red");
+        drawTrajectory(0.0, 1.0, 100, C2, TView_Proj_Cam, "gray");
+        drawTrajectory(0.0, 1.0, 100, C3, TView_Proj_Cam, "orange");
+        drawTrajectory(0.0, 1.0, 100, C4, TView_Proj_Cam, "cyan");
+        //drawRocket("red", TView_Proj_Cam_Model);
+        drawObject("silver", TView_Proj_Cam_Model, 70);
 
-        tparam += 0.03;
+        tparam += 0.01;
         if (tparam >= 5) {
             tparam = 0;
         }
+
         window.requestAnimationFrame(draw);
 
     }
